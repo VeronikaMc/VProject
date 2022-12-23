8df4ad81d7c1477c7c43061c76a8b515e8d3874d
serhanalp <serhanalpaydin@gmail.com>
2022-12-22 20:15:34 -0600

    added SerhanTest3

---
+++ src/LogOutFunctionality/SerhanTest3.java
@@ -0,0 +1,18 @@
+package LogOutFunctionality;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SerhanTest3 {
+    public static void main(String[] args) {
+
+        ArrayList <Integer> list = new ArrayList<>(Arrays.asList(2,3,4,5,6));
+
+        list.add(0,1);
+
+
+
+
+
+    }
+}
