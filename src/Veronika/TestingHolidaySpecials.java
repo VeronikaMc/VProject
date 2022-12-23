bf4384b3308cfe729433a612925e7e8379b1cf4e
vpmc1 <vmccann1128@gmail.com>
2022-12-22 21:27:29 -0500

    test: qa-333 Testing Holiday Specials

---
+++ src/Veronika/TestingHolidaySpecials.java
@@ -0,0 +1,7 @@
+package Veronika;
+
+public class TestingHolidaySpecials {
+    public static void main(String[] args) {
+        System.out.println("Xmas Specials are here-don't miss it!");
+    }
+}
