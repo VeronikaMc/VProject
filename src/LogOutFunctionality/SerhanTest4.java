0c48c0ad3b94f3107e9ee5c1057dcfe8567eba67
serhanalp <serhanalpaydin@gmail.com>
2022-12-22 20:31:38 -0600

    added SerhanTest4

---
+++ src/LogOutFunctionality/SerhanTest4.java
@@ -0,0 +1,16 @@
+package LogOutFunctionality;
+
+public class SerhanTest4 {
+    public static void main(String[] args) {
+
+
+        try{
+            String str = null;
+
+        }catch(NullPointerException e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
