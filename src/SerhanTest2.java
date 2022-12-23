8e88d79ce9d540a5c20d7b806f507bee2a8e9f8e
serhanalp <serhanalpaydin@gmail.com>
2022-12-22 19:31:35 -0600

    added one more file

---
+++ src/SerhanTest2.java
@@ -0,0 +1,14 @@
+public class SerhanTest2 {
+    public static void main(String[] args) {
+
+        String[] teamAzha = {"Serhan","Veronika","Mahmut","Elena","Merve","Victor","Maierdan","Daniel","Alan"};
+
+        for (String each : teamAzha) {
+            System.out.println(each);
+        }
+
+
+
+
+    }
+}
