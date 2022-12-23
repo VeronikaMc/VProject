78c8857f9dee52c99f281c8bc151074826ea92ae
serhanalp <serhanalpaydin@gmail.com>
2022-12-22 19:31:20 -0600

    added another file

---
+++ src/SerhanTest1.java
@@ -0,0 +1,11 @@
+public class SerhanTest1 {
+    public static void main(String[] args) {
+
+        if (1+1==2){
+            System.out.println("Vproject is the best ever");
+        }else {
+            System.out.println("There is no else");
+        }
+
+    }
+}
